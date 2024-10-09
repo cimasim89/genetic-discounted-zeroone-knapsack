5b75b31128420bea0401789a8e1f3e225d8447a4
simone <s.cimarelli@spaziocodice.com>
2024-10-09 16:18:47 +0200

    feat: refactor code
    ref #3

--- src/genetic/mod.rs
+++ src/genetic/mod.rs
@@ -1,6 +1,3 @@
-use rand::rngs::SmallRng;
-use rand::Rng;
-
 use crate::generator::{EnhancedChromosomeGenerator, RandomChromosomeGenerator};
 use crate::preprocessing::PreprocessingResult;
 use crate::structure::chromosome::Chromosome;
@@ -10,6 +7,8 @@ use crate::structure::problem::Problem;
 use crate::structure::solution::Solution;
 use crate::utils;
 use log::{debug, info};
+use rand::rngs::SmallRng;
+use rand::Rng;
 
 pub trait GeneticAlgorithm<'a> {
     fn init(problem: Problem, configuration: Box<dyn Configuration>, preprocessing_result: &'a PreprocessingResult) -> Self;
@@ -42,39 +41,32 @@ impl<'a> KnapsackGeneticAlgorithm<'a> {
     }
 
     fn get_chromosome_summary(&self, chromosome: &Chromosome) -> (i64, i64) {
-        let mut gain = 0;
-        let mut cost = 0;
+        let (mut gain, mut cost) = (0, 0);
         for (i, gene) in chromosome.genes.iter().enumerate() {
-            if *gene == 0 {
-                continue;
+            if *gene != 0 {
+                gain += self.problem.data[i][*gene - 1].gain;
+                cost += self.problem.data[i][*gene - 1].cost;
             }
-
-            gain += self.problem.data[i][*gene - 1].gain;
-            cost += self.problem.data[i][*gene - 1].cost;
         }
         (gain, cost)
     }
 
     fn find_max_rate(&self, chromosome: &Chromosome) -> (usize, usize) {
         let mut max_rate = 0.0;
-        let mut max_value: usize = 1;
-        let mut max_gene: usize = 0;
+        let mut max_gene = 0;
+        let mut max_value = 1;
         for (gene, value) in chromosome.genes.iter().enumerate() {
-            if *value == 0 {
-                continue;
-            }
-
-            let curr_rate = self.problem.data[gene][*value - 1].rate;
-
-            if curr_rate > max_rate {
-                max_rate = curr_rate;
-                max_gene = gene;
-                max_value = *value
+            if *value != 0 {
+                let curr_rate = self.problem.data[gene][*value - 1].rate;
+                if curr_rate > max_rate {
+                    max_rate = curr_rate;
+                    max_gene = gene;
+                    max_value = *value;
+                }
             }
         }
         (max_gene, max_value)
     }
-
 
     fn repair_chromosome(&self, chromosome: &Chromosome) -> Chromosome {
         let capacity = self.problem.capacity;
@@ -89,46 +81,46 @@ impl<'a> KnapsackGeneticAlgorithm<'a> {
                 cost += self.problem.data[high_rate_gene][value - 2].cost;
             }
             genes[high_rate_gene] = value - 1;
-            c = Chromosome::init_chromosome(genes.clone())
+            c = Chromosome::init_chromosome(genes.clone());
         }
         c
     }
-
 
     fn initialize_population(&mut self) {
         debug!("Initializing population...");
         let mut generated = self.configuration.get_population_size();
         let best_preprocess = self.preprocessing_result.ub_fix_result.x_best.clone();
 
-        if best_preprocess.len() > 0 {
+        if !best_preprocess.is_empty() {
             let mut chromosome = KnapsackGeneticAlgorithm::map_preprocessed_item_to_chromosome(best_preprocess);
             chromosome = self.repair_chromosome(&chromosome);
             self.population.push(chromosome);
             generated -= 1;
         }
 
-        let mut enanched_gen = EnhancedChromosomeGenerator::new(self.problem.clone(),
-                                                                self.configuration.get_seed(),
-                                                                self.preprocessing_result.relaxation_result.clone(),
-                                                                self.preprocessing_result.ub_fix_result.clone());
+        let mut enhanced_gen = EnhancedChromosomeGenerator::new(
+            self.problem.clone(),
+            self.configuration.get_seed(),
+            self.preprocessing_result.relaxation_result.clone(),
+            self.preprocessing_result.ub_fix_result.clone(),
+        );
+
         while generated > (self.configuration.get_population_size() as f64 * 0.9).floor() as u32 {
-            let mut chromosome = enanched_gen.generate_chromosome_f1();
+            let mut chromosome = enhanced_gen.generate_chromosome_f1();
             chromosome = self.repair_chromosome(&chromosome);
             self.population.push(chromosome);
             generated -= 1;
         }
 
         while generated > (self.configuration.get_population_size() as f64 * 0.9).floor() as u32 {
-            let mut chromosome = enanched_gen.generate_chromosome_f0();
+            let mut chromosome = enhanced_gen.generate_chromosome_f0();
             chromosome = self.repair_chromosome(&chromosome);
             self.population.push(chromosome);
             generated -= 1;
         }
-
 
         while generated > 0 {
-            let mut chromosome =
-                RandomChromosomeGenerator::new(self.problem.clone(), self.configuration.get_seed()).generate_chromosome();
+            let mut chromosome = RandomChromosomeGenerator::new(self.problem.clone(), self.configuration.get_seed()).generate_chromosome();
             chromosome = self.repair_chromosome(&chromosome);
             self.population.push(chromosome);
             generated -= 1;
@@ -136,8 +128,7 @@ impl<'a> KnapsackGeneticAlgorithm<'a> {
     }
 
     fn map_preprocessed_item_to_chromosome(best_preprocess: Vec<[f64; 3]>) -> Chromosome {
-        let mut genes = Vec::new();
-        best_preprocess.iter().for_each(|x| {
+        let genes = best_preprocess.iter().map(|x| {
             let mut value = 3;
             for v in x.iter().rev() {
                 if *v == 1.0 {
@@ -145,21 +136,19 @@ impl<'a> KnapsackGeneticAlgorithm<'a> {
                 }
                 value -= 1;
             }
-            genes.push(value);
-        });
+            value
+        }).collect();
         Chromosome::init_chromosome(genes)
     }
 
-
     fn make_solution(&mut self, chromosome: &Chromosome, generations: u32) -> Solution {
-        let mut data: Vec<Item> = Vec::new();
+        let mut data = Vec::new();
         let mut cost = 0;
         for (gene, value) in chromosome.genes.iter().enumerate() {
-            if *value == 0 {
-                continue;
+            if *value != 0 {
+                data.push(self.problem.data[gene][*value - 1].clone());
+                cost += self.problem.data[gene][*value - 1].cost;
             }
-            data.push(self.problem.data[gene][*value - 1].clone());
-            cost += self.problem.data[gene][*value - 1].cost;
         }
         Solution::make_solution(data, chromosome.fitness, cost, generations)
     }
@@ -173,59 +162,42 @@ impl<'a> KnapsackGeneticAlgorithm<'a> {
     }
 
     fn evaluate(&mut self) {
-        debug!("evaluating population...");
-
-        let fitness: Vec<_> = self.population.iter().map(|c| {
-            self.fitness_func(c)
-        }).collect();
-
+        debug!("Evaluating population...");
+        let fitness: Vec<_> = self.population.iter().map(|c| self.fitness_func(c)).collect();
         for (i, chromosome) in self.population.iter_mut().enumerate() {
             chromosome.set_fitness(fitness[i]);
             chromosome.increase_age();
         }
-
         self.population.sort_by(|a, b| b.fitness.cmp(&a.fitness));
     }
 
     fn roulette_wheel_selection(&mut self) {
-        let sum_fitness = self.population.iter().fold(0 as i64, |acc, c| acc + c.fitness);
-
-        self.population = (0..self.population.len())
-            .map(|_| {
-                let mut slice = self.rng.gen_range(0..sum_fitness);
-                let mut index = 0;
-                for chromosome in self.population.iter() {
-                    slice -= chromosome.fitness;
-                    if slice <= 0 {
-                        break;
-                    }
-                    index += 1;
+        let sum_fitness = self.population.iter().map(|c| c.fitness).sum();
+        self.population = (0..self.population.len()).map(|_| {
+            let mut slice = self.rng.gen_range(0..sum_fitness);
+            let mut index = 0;
+            for chromosome in self.population.iter() {
+                slice -= chromosome.fitness;
+                if slice <= 0 {
+                    break;
                 }
-                self.population[index].clone()
-            })
-            .collect()
+                index += 1;
+            }
+            self.population[index].clone()
+        }).collect();
     }
 
     fn select(&mut self) {
         debug!("Selecting population...");
-        self.roulette_wheel_selection()
+        self.roulette_wheel_selection();
     }
 
     fn parent_crossover(&mut self, parent1: &Chromosome, parent2: &Chromosome) -> (Chromosome, Chromosome) {
         let crossover_point = self.rng.gen_range(0..parent1.genes.len());
-
-        let mut child1_genes = Vec::new();
-        let mut child2_genes = Vec::new();
-
-        for i in 0..parent1.genes.len() {
-            if i < crossover_point {
-                child1_genes.push(parent1.genes[i]);
-                child2_genes.push(parent2.genes[i]);
-            } else {
-                child1_genes.push(parent2.genes[i]);
-                child2_genes.push(parent1.genes[i]);
-            }
-        }
+        let (child1_genes, child2_genes): (Vec<_>, Vec<_>) = parent1.genes.iter().zip(&parent2.genes)
+            .enumerate()
+            .map(|(i, (g1, g2))| if i < crossover_point { (*g1, *g2) } else { (*g2, *g1) })
+            .unzip();
 
         let mut child1 = Chromosome::init_chromosome(child1_genes);
         child1 = self.repair_chromosome(&child1);
@@ -237,34 +209,27 @@ impl<'a> KnapsackGeneticAlgorithm<'a> {
 
     fn crossover(&mut self) {
         debug!("Crossover population...");
-
         let mut new_population = Vec::new();
-
         for _ in 0..self.population.len() / 2 {
             let parent1 = self.population[self.rng.gen_range(0..self.population.len())].clone();
             let parent2 = self.population[self.rng.gen_range(0..self.population.len())].clone();
-
             let (child1, child2) = self.parent_crossover(&parent1, &parent2);
-
-            new_population.push(child1.clone());
-            new_population.push(child2.clone());
+            new_population.push(child1);
+            new_population.push(child2);
         }
-
         self.population = new_population;
     }
 
     fn mutate(&mut self) {
         debug!("Mutating population...");
-
         self.population.iter_mut().for_each(|c| {
-            if self.rng.gen_range(0..1000) > self.mutation_factor {
-                return;
+            if self.rng.gen_range(0..1000) <= self.mutation_factor {
+                let index = self.rng.gen_range(0..c.genes.len());
+                let gene = self.rng.gen_range(0..4);
+                c.genes[index] = gene;
+                c.fitness = 0;
+                c.age = 0;
             }
-            let index = self.rng.gen_range(0..c.genes.len());
-            let gene = self.rng.gen_range(0..4);
-            c.genes[index] = gene;
-            c.fitness = 0;
-            c.age = 0;
         });
     }
 
@@ -274,47 +239,30 @@ impl<'a> KnapsackGeneticAlgorithm<'a> {
             self.remain_no_improved_generations = self.configuration.get_no_upgrade_limit();
             return false;
         }
-
         if self.remain_no_improved_generations > 0 {
             self.remain_no_improved_generations -= 1;
             return false;
         }
-
         true
     }
 
     fn evolve(&mut self) -> (Chromosome, u32) {
         let mut generation: u32 = 0;
-        let mut condition = true;
-
-        while condition {
+        self.evaluate();
+        while !self.check_is_end(self.population.first().cloned().unwrap_or_else(|| panic!("Problem occurs during evolution!"))) {
             info!("Evolving population generation: {} current best fitness: {}", generation, self.best.fitness);
-            self.evaluate();
-            let current_best = match self.population.first() {
-                None => { panic!("Problem occurs during evolution!") }
-                Some(c) => {
-                    c.clone()
-                }
-            };
-
-            if self.check_is_end(current_best) {
-                condition = false;
-            } else {
-                self.select();
-                self.crossover();
-                self.mutate();
-                generation += 1;
+            self.select();
+            self.crossover();
+            self.mutate();
+            generation += 1;
+            if generation % 10 == 0 && self.mutation_factor > 1 {
+                self.mutation_factor -= 1;
             }
+            self.evaluate();
         }
-
-        if generation % 10 == 0 && self.mutation_factor > 1 {
-            self.mutation_factor -= 1;
-        }
-
         (self.best.clone(), generation)
     }
 }
-
 
 impl<'a> GeneticAlgorithm<'a> for KnapsackGeneticAlgorithm<'a> {
     fn init(problem: Problem, configuration: Box<dyn Configuration>, preprocessing_result: &'a PreprocessingResult) -> Self {
@@ -324,35 +272,8 @@ impl<'a> GeneticAlgorithm<'a> for KnapsackGeneticAlgorithm<'a> {
     }
 
     fn run(&mut self) -> Solution {
-        info!("Running genetic algorithm for knapsack capacity: {}, selection size: {} ",
-                 self.problem.capacity,
-                 self.problem.size);
+        info!("Running genetic algorithm for knapsack capacity: {}, selection size: {} ", self.problem.capacity, self.problem.size);
         let (best, generations) = self.evolve();
         self.make_solution(&best, generations)
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::genetic::KnapsackGeneticAlgorithm;
-
-    #[test]
-    fn test_map_preprocessed_item_to_chromosome() {
-        let best_preprocess = vec![
-            [1.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 0.0],
-        ];
-
-        let chromosome = KnapsackGeneticAlgorithm::map_preprocessed_item_to_chromosome(best_preprocess);
-        let expected_genes = vec![1, 2, 3, 0];
-
-        assert_eq!(chromosome.genes, expected_genes);
-    }
-}
-
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use std::time::SystemTime;
-
 use crate::genetic::{GeneticAlgorithm, KnapsackGeneticAlgorithm};
 use crate::parser::*;
-use crate::preprocessing::ProblemPreprocessor;
+use crate::preprocessing::{PreprocessingResult, ProblemPreprocessor};
 use crate::report::Report;
 use crate::structure::configuration::ConfigurationByGenerations;
 use crate::structure::problem::Problem;
 use clap::Parser;
 use env_logger::Env;
 use log::{debug, info};
+use std::time::SystemTime;
 use uuid::Uuid;
 
 mod parser;
@@ -41,32 +40,30 @@ struct Args {
     log_level: String,
 }
 
+fn parse_args() -> Args {
+    Args::parse()
+}
 
-fn main() {
-    let args = Args::parse();
-    env_logger::Builder::from_env(Env::default().default_filter_or(args.log_level)).init();
-    let problem = <Problem as ProblemParser>::parse_input(args.file_path.clone());
-    let population_size = problem.size as u32 * 5;
-    let result_path = match args.result_file_name.as_str() {
-        "" => "metrics.csv".to_string(),
-        _ => args.result_file_name,
-    };
-    let csv = report::CSV {
-        path: result_path,
-    };
+fn initialize_problem(file_path: &str) -> Problem {
+    <Problem as ProblemParser>::parse_input(file_path.to_string())
+}
 
+fn initialize_preprocessor(problem: &Problem) -> ProblemPreprocessor {
+    ProblemPreprocessor::new(problem)
+}
 
-    let mut preprocessor = ProblemPreprocessor::new(&problem);
-    let preprocessing_result = preprocessor.process_problem();
+fn initialize_configuration(args: &Args, seed: u64, population_size: u32) -> ConfigurationByGenerations {
+    ConfigurationByGenerations {
+        initial_mutation_factor: args.initial_mutation_factor,
+        no_upgrade_limit: args.no_upgrade_limit,
+        population_size,
+        seed,
+    }
+}
 
+fn execute_algorithm(args: &Args, problem: &Problem, preprocessing_result: &PreprocessingResult, csv: &report::CSV) {
     for i in 0..args.times {
-        let configuration = ConfigurationByGenerations {
-            initial_mutation_factor: args.initial_mutation_factor,
-            no_upgrade_limit: args.no_upgrade_limit,
-            population_size,
-            seed: i,
-        };
-
+        let configuration = initialize_configuration(args, i, problem.size as u32 * 5);
         let start = SystemTime::now();
         let mut executor = <KnapsackGeneticAlgorithm as GeneticAlgorithm>::init(
             problem.clone(),
@@ -86,9 +83,21 @@ fn main() {
             args.file_path.clone(),
             i,
             args.no_upgrade_limit,
-            population_size,
+            problem.size as u32 * 5,
             &solution,
             elapsed,
         );
     }
+}
+
+fn main() {
+    let args = parse_args();
+    env_logger::Builder::from_env(Env::default().default_filter_or(&args.log_level)).init();
+    let problem = initialize_problem(&args.file_path);
+    let mut preprocessor = initialize_preprocessor(&problem);
+    let csv = report::CSV {
+        path: if args.result_file_name.is_empty() { "metrics.csv".to_string() } else { args.result_file_name.clone() },
+    };
+    let preprocessing_result = preprocessor.process_problem();
+    execute_algorithm(&args, &problem, &preprocessing_result, &csv);
 }
